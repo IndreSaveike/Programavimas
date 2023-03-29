7af4c68edcd92484e4215e0e1199ca17645cfaf6
IndreSaveike <indre.saveike@gmail.com>
2023-03-29 20:48:36 +0300

    Add solution for task uzduotis 2

--- uzduotis02/Program.cs
+++ uzduotis02/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿Console.Write("----------------------------------");
+Console.Write("\n|                                |");
+Console.Write("\n|         Mano vardas            |");
+Console.Write("\n|                                |");
+Console.Write("\n|           Indre                |");
+Console.Write("\n|                                |");
+Console.WriteLine("\n----------------------------------");
