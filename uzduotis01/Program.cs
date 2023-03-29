b4aca89bfa8de755972a7b313ec9816141fdffb9
IndreSaveike <indre.saveike@gmail.com>
2023-03-29 20:36:18 +0300

    Add solution for task uzduotis 1

---
+++ uzduotis01/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name = "Indre";
+        Console.WriteLine("Labas, " + name);
+
+        Console.Write("Įvestas amžius:");
+
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.ReadLine();
+
+        int num = 25;
+        Console.Write(num);
+        Console.Write(num);
+        Console.Write(num);
+        Console.Write(num);
+        Console.Write(num + "\n");
+
+        Console.WriteLine(+num + " " + num + " " + num + " " + num + " " + num);
+    }
+}
