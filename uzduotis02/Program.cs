1c478707e3b46d8d31ef724fc517f1c64936729e
IndreSaveike <indre.saveike@gmail.com>
2023-03-29 21:00:19 +0300

    Update Program.cs

--- uzduotis02/Program.cs
+++ uzduotis02/Program.cs
@@ -1,7 +1,14 @@
-﻿Console.Write("----------------------------------");
-Console.Write("\n|                                |");
-Console.Write("\n|         Mano vardas            |");
-Console.Write("\n|                                |");
-Console.Write("\n|           Indre                |");
-Console.Write("\n|                                |");
-Console.WriteLine("\n----------------------------------");
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("----------------------------------");
+        Console.Write("\n|                                |");
+        Console.Write("\n|         Mano vardas            |");
+        Console.Write("\n|                                |");
+        Console.Write("\n|           Indre                |");
+        Console.Write("\n|                                |");
+        Console.WriteLine("\n----------------------------------");
+    }
+}
