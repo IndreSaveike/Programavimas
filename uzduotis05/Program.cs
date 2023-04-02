eb8329fdc9f626b660f647795a93ca87d4ba2bce
IndreSaveike <indre.saveike@gmail.com>
2023-04-02 15:21:23 +0300

    Add soliution aritmetiniai veiksmai

---
+++ uzduotis05/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace uzduotis05;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Pirma užduotis");
+        Console.WriteLine();
+
+        double a = 4;
+        double b = 5;
+   
+
+        double suma = a + b;
+        double skirtumas = a - b;
+        double sandauga = a * b;
+        double dalmuo = a / b;
+
+
+        Console.WriteLine($"{a} + {b} = {suma}");
+        Console.WriteLine($"{a} - {b} = {skirtumas}");
+        Console.WriteLine($"{a} * {b} = {sandauga}");
+        Console.WriteLine($"{a} / {b} = {dalmuo}");
+
+        Console.WriteLine();
+        Console.WriteLine( "Antra užduotis");
+        Console.WriteLine();
+
+
+        double kvadratas = Math.Pow(a, 2);
+        double treciojoLaipsnio = Math.Pow(a, 3);
+
+        
+        Console.WriteLine($"Skaičius: {a}");
+        Console.WriteLine($"Kvadratu: {kvadratas}");
+        Console.WriteLine($"Trečiuoju laipsniu: {treciojoLaipsnio}");
+
+        Console.WriteLine();
+        Console.WriteLine("Trečia užduotis");
+        Console.WriteLine();
+
+        double c = 6;
+
+        double sandauga1 = a * b * c;
+        Console.WriteLine($"Sandauga: {a} * {b} * {c} = {sandauga1}");
+
+
+
+
+
+
+
+    }
+}
---
+++ uzduotis06/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace uzduotis06;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Pirma užduotis:");
+
+
+        int skaicius = 5;
+
+        Console.WriteLine($"Daugybos lentelė {skaicius}:");
+
+        for (int i = 0; i <= 10; i++)
+        {
+            int rezultatas = skaicius * i;
+            Console.WriteLine($"{skaicius} x {i} = {rezultatas}");
+        }
+
+        
+        Console.WriteLine();
+        Console.WriteLine("antra užduotis:");
+        Console.WriteLine();
+
+
+        int skaicius1 = 37;
+
+        int pirmasSkaitmuo1 = skaicius1 / 10;
+        int antrasSkaitmuo1 = skaicius1 % 10;
+        int sandauga = pirmasSkaitmuo1 * antrasSkaitmuo1;
+
+        
+        Console.WriteLine($"Dviženklis skaičius: {skaicius1}");
+        Console.WriteLine($"Skaitmenų sandauga: {pirmasSkaitmuo1} * {antrasSkaitmuo1} = {sandauga}");
+    }
+}
