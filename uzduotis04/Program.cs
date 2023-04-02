78141eb2351434eb3005cff602d9562b92d2562d
IndreSaveike <indre.saveike@gmail.com>
2023-04-02 15:20:25 +0300

    Add  solution for task uzduotis04

---
+++ uzduotis04/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace uzduotis04;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string marke = "Mercedes_Benz";
+        string modelis = "Gle";
+        int metai = 2023;
+        int rida = 10000;
+
+        Console.WriteLine($"Automobilio {marke} {modelis} ({metai} m.) rida - {rida} km.");
+    }
+}
+
+
