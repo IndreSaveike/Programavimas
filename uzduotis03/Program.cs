b9c0c3af0fd5549ec3d2246ee7cb55d4c4c87d9f
IndreSaveike <indre.saveike@gmail.com>
2023-04-02 15:17:24 +0300

    add solution for task uzduotis03

---
+++ uzduotis03/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace uzduotis03;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("****");
+        Console.WriteLine("*  *");
+        Console.WriteLine("*  *");
+        Console.WriteLine("****");
+    
+    }
+}
+
