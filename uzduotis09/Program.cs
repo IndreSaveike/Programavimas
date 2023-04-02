5a0d00f99302879dbbbce1684b725683729d2fa6
IndreSaveike <indre.saveike@gmail.com>
2023-04-02 15:25:15 +0300

    Add solution patikrinimo salyga if

---
+++ uzduotis09/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace uzduotis09;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Tikriname ar vartotojas gali balsuoti
+        Console.Write("Įveskite savo amžių: ");
+        int age = int.Parse(Console.ReadLine());
+
+        if (age >= 18)
+        {
+            Console.WriteLine("Jūs galite balsuoti");
+        }
+        else
+        {
+            Console.WriteLine("Deja, jūs negalite balsuoti.");
+        }
+
+        // Patikriname įvestą skaičių
+        Console.Write("Įveskite bet kokį skaičių: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number < 0)
+        {
+            Console.WriteLine("Negalima vesti neigiamo skaičiaus :)");
+        }
+        else if (number > 0)
+        {
+            Console.WriteLine("Sveikiname! Jūsų skaičius yra teigiamas.");
+        }
+        else
+        {
+            Console.WriteLine("Jūsų skaičius yra lygus 0.");
+        }
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Taip, skaičius yra lyginis.");
+        }
+
+        if (number % 4 == 0)
+        {
+            Console.WriteLine("Taip, skaičius dalinasi iš 4.");
+        }
+
+        if (number > 10)
+        {
+            Console.WriteLine("Taip, skaičius yra didesnis už 10.");
+        }
+
+        // Pažymių vidurkio skaičiavimas ir tikrinimas
+        Console.Write("Įveskite du savo pažymius, atskirtus kableliu: ");
+        string[] grades = Console.ReadLine().Split(',');
+        double sum = 0;
+        for (int i = 0; i < grades.Length; i++)
+        {
+            double parsedGrade;
+            if (double.TryParse(grades[i], out parsedGrade))
+            {
+                sum += parsedGrade;
+            }
+            else
+            {
+                Console.WriteLine("Netinkamas pažymio formatas: {0}", grades[i]);
+            }
+        }
+        double average = sum / grades.Length;
+        Console.WriteLine("Pažymių vidurkis: {0}", average);
+        if (average >= 2)
+        {
+            Console.WriteLine("Valio!!!");
+        }
+    }
+}
---
+++ uzduotis10/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace uzduotis10;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Įveskite skaičių: ");
+        int skaicius = int.Parse(Console.ReadLine());
+
+        if (skaicius % 2 == 0)
+        {
+            int penktasis_laipsnis = (int)Math.Pow(skaicius, 5);
+            Console.WriteLine("{0} {1}", skaicius, penktasis_laipsnis);
+        }
+        else if (skaicius % 4 != 0)
+        {
+            Console.Write("Įveskite antrąjį skaičių: ");
+            int antras_skaicius = int.Parse(Console.ReadLine());
+
+            int suma = skaicius + antras_skaicius;
+            int sandauga = skaicius * antras_skaicius;
+            Console.WriteLine("{0} + {1} = {2}", skaicius, antras_skaicius, suma);
+            Console.WriteLine("{0} * {1} = {2}", skaicius, antras_skaicius, sandauga);
+        }
+    }
+}
+
