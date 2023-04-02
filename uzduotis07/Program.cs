bd7af28475f342b3771082ca6846b10a5680f44a
IndreSaveike <indre.saveike@gmail.com>
2023-04-02 15:23:42 +0300

    Add solution informacijos ivedimas

---
+++ uzduotis07/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace uzduotis07;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 1. Vartotojo vardo ir amžiaus įvedimas
+        Console.Write("Įveskite savo vardą: ");
+        string vardas = Console.ReadLine();
+
+        Console.Write("Įveskite savo amžių: ");
+        int amzius = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Jūsų vardas {vardas}, o amžius {amzius}.");
+        Console.WriteLine();
+
+        // 2. Simbolio įvedimas ir 3x3 kvadrato atspausdinimas
+        Console.Write("Įveskite bet kokį simbolį: ");
+        char simbolis = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+        Console.WriteLine($"{simbolis}{simbolis}{simbolis}");
+        Console.WriteLine($"{simbolis}{simbolis}{simbolis}");
+        Console.WriteLine($"{simbolis}{simbolis}{simbolis}");
+        Console.WriteLine();
+
+        // 3. Skaičiaus įvedimas ir kvadrato atspausdinimas
+        Console.Write("Įveskite bet kokį skaičių: ");
+        double skaicius = Convert.ToDouble(Console.ReadLine());
+        double kvadratas = Math.Pow(skaicius, 2);
+        Console.WriteLine($"Skaičiaus {skaicius} kvadratas: {kvadratas}");
+        Console.WriteLine();
+
+        // 4. Trijų skaičių įvedimas ir operacijų atlikimas
+        Console.Write("Įveskite pirmąjį skaičių: ");
+        double sk1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Įveskite antrąjį skaičių: ");
+        double sk2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Įveskite trečiąjį skaičių: ");
+        double sk3 = Convert.ToDouble(Console.ReadLine());
+
+        double suma = sk1 + sk2 + sk3;
+        double skirtumas = sk1 - sk2 - sk3;
+        double sandauga = sk1 * sk2 * sk3;
+        double dalmuo = sk1 / sk2 / sk3;
+
+        Console.WriteLine($"{sk1} + {sk2} + {sk3} = {suma}");
+        Console.WriteLine($"{sk1} - {sk2} - {sk3} = {skirtumas}");
+        Console.WriteLine($"{sk1} * {sk2} * {sk3} = {sandauga}");
+        Console.WriteLine($"{sk1} / {sk2} / {sk3} = {dalmuo}");
+    }
+}
---
+++ uzduotis08/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace uzduotis08;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 1. Skaičiaus įvedimas ir daugybos lentelės atspausdinimas nuo 1 iki 5
+        Console.Write("Įveskite bet kokį sveikąjį skaičių: ");
+        int skaicius = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Daugybos lentelė {skaicius}:");
+        for (int i = 1; i <= 5; i++)
+        {
+            int rezultatas = skaicius * i;
+            Console.WriteLine($"{skaicius} * {i} = {rezultatas}");
+        }
+        Console.WriteLine();
+
+        // 2. Trijų skaičių įvedimas ir vidurkio atspausdinimas
+        Console.Write("Įveskite pirmąjį skaičių: ");
+        double sk1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Įveskite antrąjį skaičių: ");
+        double sk2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Įveskite trečiąjį skaičių: ");
+        double sk3 = Convert.ToDouble(Console.ReadLine());
+
+        double vidurkis = (sk1 + sk2 + sk3) / 3;
+        Console.WriteLine($"Įvestų skaičių {sk1}, {sk2}, {sk3} vidurkis: {vidurkis}");
+        Console.WriteLine();
+
+        // 3. Skaičių įvedimas, kvadratų, trečiųjų laipsnių ir dalijimo iš 2 atspausdinimas
+        Console.Write("Įveskite pirmąjį skaičių: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Įveskite antrąjį skaičių: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Įveskite trečiąjį skaičių: ");
+        double num3 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"Skaičius {num1}: kvadratu {Math.Pow(num1, 2)}, trečiuoju laipsniu {Math.Pow(num1, 3)}, padalintas iš dviejų {num1 / 2}");
+        Console.WriteLine($"Skaičius {num2}: kvadratu {Math.Pow(num2, 2)}, trečiuoju laipsniu {Math.Pow(num2, 3)}, padalintas iš dviejų {num2 / 2}");
+        Console.WriteLine($"Skaičius {num3}: kvadratu {Math.Pow(num3, 2)}, trečiuoju laipsniu {Math.Pow(num3, 3)}, padalintas iš dviejų {num3 / 2}");
+    }
+}
