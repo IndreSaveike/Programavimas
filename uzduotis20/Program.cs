bc17d5ed4c85ea52b791982f8e1367e2554068ba
IndreSaveike <indre.saveike@gmail.com>
2023-04-15 20:20:09 +0300

    Add branch for uzduotis20

---
+++ uzduotis20/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace uzduotis20;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Įveskite iš konsolės 2 skaičius (stataus trikampio statinių ilgius).
+        Console.Write("Įveskite stačiojo trikampio pirmos statinės ilgį: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Įveskite stačiojo trikampio antros statinės ilgį: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+        double c = Math.Sqrt(a * a + b * b);
+        Console.WriteLine("Stačiojo trikampio įžambinė: " + c);
+    }
+}
+
