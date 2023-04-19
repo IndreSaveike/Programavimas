1d4193efec4d9ff28742fa14a7881e5afab317e6
IndreSaveike <indre.saveike@gmail.com>
2023-04-19 22:44:40 +0300

    Add uzduotis35

---
+++ uzduotis35/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace uzduotis35;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Sukurti void tipo metodą, kuriam paduosim masyvą.
+        //Metodas turi išvesti masyvo sumą(sumai skaičiuoti naudoti for ciklą ir .Sum()  funkciją.
+        //Tuomet metodas išves į konsolę 2 tokias pačias sumas suskaičiuotas  skirtingais būdais)
+        //Išsikviesti metodą.
+
+        int[] masyvas = { 1, 2, 3, 4, 5 };
+        IsvestiSuma(masyvas);
+    }
+
+    static void IsvestiSuma(int[] masyvas)
+    {
+        int suma1 = 0;
+        for (int i = 0; i < masyvas.Length; i++)
+        {
+            suma1 += masyvas[i];
+        }
+
+        int suma2 = masyvas.Sum();
+
+        Console.WriteLine($"Suma: {suma1}");
+        Console.WriteLine($"Suma: {suma2}");
+    }
+}
+   
