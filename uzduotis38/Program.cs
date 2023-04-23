47f47ebb184ef6b7c37f7e47b22f685d0f3438f3
IndreSaveike <indre.saveike@gmail.com>
2023-04-23 08:22:31 +0300

    add uzduotis38

---
+++ uzduotis38/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace uzduotis38;
+//1.Sukurkite bazinę klasę Gyvunas su protected kintamaisiais ir paveldinčia
+//klasę Gyvate.Paveldinčioje klasėje turi būti metodas, kuris išveda visus klasės  kintamuosius į ekraną.
+//2.Bazinės klasės konstruktorių panaudokite paveldinčioje klasėje.
+//3.Išveskite paveldinčios klasės kintamuosius į ekraną.
+public class Gyvunas
+{
+    protected string rusis;
+    protected int amzius;
+
+    public Gyvunas(string rusis, int amzius)
+    {
+        this.rusis = rusis;
+        this.amzius = amzius;
+    }
+}
+
+public class Gyvate : Gyvunas
+{
+    public Gyvate(string rusis, int amzius) : base(rusis, amzius)
+    {
+    }
+
+    public void IsvestiInformacija()
+    {
+        Console.WriteLine($"Rūšis: {rusis}");
+        Console.WriteLine($"Amžius: {amzius}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Gyvate gyvate = new Gyvate("Gyvatė", 2);
+        gyvate.IsvestiInformacija();
+    }
+}
