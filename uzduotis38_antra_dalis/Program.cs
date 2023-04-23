65dd64c6b95ea3710832f74db92252430eecf823
IndreSaveike <indre.saveike@gmail.com>
2023-04-23 10:12:47 +0300

    Add uzduotis38_antra_dalis

---
+++ uzduotis38_antra_dalis/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace uzduotis38_antra_dalis;
+public class Gyvunas
+{
+    protected string rusis;
+    protected int amzius;
+    private const string PrivateConst = "Gyvūnų klasės konstanta.";
+
+    public Gyvunas(string rusis, int amzius)
+    {
+        this.rusis = rusis;
+        this.amzius = amzius;
+    }
+
+    public virtual void Isvedimas()
+    {
+        Console.WriteLine("Base klasės išvedimo metodas.");
+    }
+
+    public void SpausdintiPrivateConst()
+    {
+        Console.WriteLine(PrivateConst);
+    }
+}
+
+public class Gyvate : Gyvunas
+{
+    public Gyvate(string rusis, int amzius) : base(rusis, amzius)
+    {
+    }
+
+    public override void Isvedimas()
+    {
+        Console.WriteLine($"Rūšis: {rusis}");
+        Console.WriteLine($"Amžius: {amzius}");
+        SpausdintiPrivateConst();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Gyvate gyvate = new Gyvate("Gyvatė", 2);
+        gyvate.Isvedimas();
+    }
+}
