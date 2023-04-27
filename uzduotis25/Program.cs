c2e80fbf6266f8092847ba2c0ee3f1d44c94767e
IndreSaveike <indre.saveike@gmail.com>
2023-04-27 19:05:07 +0300

    Add uzduotis25

---
+++ uzduotis25/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace uzduotis25;
+//Liepti vartotojui įvesti 5 pažymius VIENOJE eilutėje atskiriant tarpu.Rasti didžiausią pažymį.
+//Liepti vartotojui įvesti 5 pažymius VIENOJE eilutėje atskiriant kableliu.Rasti
+//mažiausią pažymį.
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Įveskite 5 pažymius atskiriant tarpu:");
+        string[] pazymiaiSuTarpu = Console.ReadLine().Split(' ');
+
+        int[] pazymiaiSuTarpuSkaiciai = Array.ConvertAll(pazymiaiSuTarpu, int.Parse);
+        int didziausiasPazymys = pazymiaiSuTarpuSkaiciai.Max();
+        Console.WriteLine("Didžiausias pažymys: " + didziausiasPazymys);
+
+        Console.WriteLine("Įveskite 5 pažymius atskiriant kableliu:");
+        string[] pazymiaiSuKableliu = Console.ReadLine().Split(',');
+
+        int[] pazymiaiSuKableliuSkaiciai = Array.ConvertAll(pazymiaiSuKableliu, int.Parse);
+        int maziausiasPazymys = pazymiaiSuKableliuSkaiciai.Min();
+        Console.WriteLine("Mažiausias pažymys: " + maziausiasPazymys);
+    }
+}
+
